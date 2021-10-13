c04abc80def7ae10c21fb764dd2a632ef62fb86a
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-13 15:57:32 -0300

    correção nome rota

--- src/routes/characteres.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import CharactereController from '../controllers/CharactereController';
-
-const routes = new Router();
-
-routes.get('/', CharactereController.index);
-
-export default routes;
