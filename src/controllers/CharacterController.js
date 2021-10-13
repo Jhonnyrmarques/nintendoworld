2679bd0e9ec30082f19b9ee2a17a2f8c1744af89
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-13 16:42:47 -0300

    adicionado o metodo para criar personagem

--- src/controllers/CharacterController.js
+++ src/controllers/CharacterController.js
@@ -1,6 +1,19 @@
+import Character from '../models/Character';
+
 class CharacterController {
   index(req, res) {
     res.send('OK');
+  }
+
+  async store(req, res) {
+    try {
+      const character = await Character.create(req.body);
+      return res.json(character);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
   }
 }
 
