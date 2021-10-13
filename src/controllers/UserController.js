aa9de986303b46f0ef25fdc499c6a5f187e2ecb3
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-13 18:18:07 -0300

    criado o método store

---
+++ src/controllers/UserController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      const { id, nome, email } = newUser;
+      return res.json({ id, nome, email });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+}
+
+export default new UserController();
