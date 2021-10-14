53720eda232774beb13fd47775d75172b1f0c9d5
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-14 16:28:48 -0300

    adicionado model de imagens

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,11 @@ import { Sequelize } from 'sequelize';
 import databaseConfig from '../config/database';
 import Character from '../models/Character';
 import User from '../models/User';
+import Image from '../models/Image';
 
 const connection = new Sequelize(databaseConfig);
 
-const models = [Character, User];
+const models = [Character, User, Image];
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
