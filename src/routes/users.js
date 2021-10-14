846e1df1d61751e77330fb1333a06f4cf532071f
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-14 14:05:39 -0300

    adicionado o middleware loginRequired

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const routes = new Router();
 
 routes.post('/', userController.store);
-routes.put('/:id', userController.update);
+routes.put('/:id', loginRequired, userController.update);
 
 export default routes;
