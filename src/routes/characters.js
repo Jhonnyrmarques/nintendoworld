e82686431ec84a8c599f1193078b575e2d0172a2
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-14 14:48:04 -0300

    aplicado login

--- src/routes/characters.js
+++ src/routes/characters.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import CharacterController from '../controllers/CharacterController';
+import loginRequired from '../middlewares/loginRequired';
 
 const routes = new Router();
 
 routes.get('/', CharacterController.index);
-routes.post('/', CharacterController.store);
+routes.post('/', loginRequired, CharacterController.store);
 routes.get('/:id', CharacterController.show);
-routes.put('/:id', CharacterController.update);
+routes.put('/:id', loginRequired, CharacterController.update);
 
 export default routes;
