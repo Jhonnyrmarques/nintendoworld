98895409f850344dbee5a2b834b8ae98717223c0
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2021-10-14 16:56:41 -0300

    adicionado model para manipulação de imagens

---
+++ src/models/Image.js
@@ -0,0 +1,40 @@
+import Sequelize, { Model } from 'sequelize';
+import appConfig from '../config/appConfig';
+
+export default class Image extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Field cannot be empty',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Field cannot be empty',
+          },
+        },
+      },
+      url: {
+        type: Sequelize.VIRTUAL,
+        get() {
+          return `${appConfig.url}/img/${this.getDataValue('filename')}`;
+        },
+      },
+    }, {
+      sequelize,
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Character, { foreignKey: 'character_id' });
+  }
+}
